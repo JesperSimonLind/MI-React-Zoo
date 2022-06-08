80b348da8bb641e3da07f636c916ecbf106d43b8
PeppZZon <jespersimonlind@gmail.com>
2022-06-08 16:39:12 +0200

    Added fetch from API

--- src/components/ShowAnimals.tsx
+++ src/components/ShowAnimals.tsx
@@ -1,3 +1,17 @@
+import { useEffect, useState } from "react";
+import { IAnimal } from "../models/IAnimal";
+
 export const ShowAnimals = () => {
+  const [animals, setAnimals] = useState<IAnimal[]>([]);
+
+  useEffect(() => {
+    if (animals.length !== 0) return;
+    fetch("https://animals.azurewebsites.net/api/animals")
+      .then((response) => response.json())
+      .then((animalData) => {
+        setAnimals(animalData);
+        console.log(animalData);
+      });
+  });
   return <>Det funkar!</>;
 };
